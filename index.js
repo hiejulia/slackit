b54e99da0b6e64878be88401667270539b5b2071
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-13 15:35:46 +0300

    update the npm scripts

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
-//import real time mes
-    
+//import real time mes client from slack api
+const RtmClient = require('@slack/client').RtmClient;
+//including rtm 
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+//import rtm events  constants from slack api
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS
+
