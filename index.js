660cf7f791c193876d4fc2143083e5a49f93e0fb
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-13 15:51:52 +0300

    bot works

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 //import client event const from slack api
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-const token = '';
+const token = 'xoxb-182470253664-BX6FLRWvCu33PNs2yWAkkJMP';
 
 //slack constructor take 2 arg > token & opts
 let slack = new RtmClient(token,{
