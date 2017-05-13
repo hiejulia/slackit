62c04fbf92ad89fbca34c003f57378595365b383
Hien Nguyen <hienminhnguyen711@gmail.com>
2017-05-13 15:40:31 +0300

    init slack bot

--- index.js
+++ index.js
@@ -4,5 +4,22 @@ const RtmClient = require('@slack/client').RtmClient;
 //including rtm 
 const MemoryDataStore = require('@slack/client').MemoryDataStore;
 //import rtm events  constants from slack api
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+//import client event const from slack api
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+const token = '';
+
+//slack constructor take 2 arg > token & opts
+let slack = new RtmClient(token,{
+    //set level of log we require
+    logLevel: 'debug', 
+    //init data store for client > load additional helper funcs for store and retrieve data
+
+    dataStore: new MemoryDataStore(),
+    //slack should auto reconnect after error res
+    autoReconnect: true,
+    //each mess > marked as read
+    autoMark: true 
+});
 
